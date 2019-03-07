9966d1dfa30cddedde4e2acd88d74122b4372a38
Bolton <bolton.otieno@gmail.com>
2019-03-07 14:45:55 +0300

    add pug view template

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
-const express = require("express")
+const express = require("express");
+const path = require("path");
 
-const app = express()
+// Init App
+const app = express();
 
+// View Engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+// Main route
 app.get("/", (req, res) => {
-    res.send("hello world")
-})
+  res.render("index", {
+    title: "Hello guys"
+  });
+});
+
+app.get("/articles/add", (req, res) => {
+  let articles = [
+    {
+      id: 1,
+      title: "some title",
+      author: "BoltC0rt3z",
+      body: "this a body"
+    },
+    {
+      id: 2,
+      title: "some title 2",
+      author: "jane",
+      body: "this a body 2"
+    },
+    {
+      id: 3,
+      title: "some title 3",
+      author: "John",
+      body: "this a body 3"
+    }
+  ]
+  res.render("add_articles", {
+    title: "Add articles",
+    articles: articles
+  });
+});
 
 // configure PORT
-PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server running on Port ${PORT}`))
+PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
